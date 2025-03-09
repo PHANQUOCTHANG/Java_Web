cbddde3c820587860b0b2290a650c7560206d458
QuocThang <phanquocthang211005@gmail.com>
2025-03-09 11:45:14 +0700

    mvc

--- src/main/java/com/javaweb/Java_web/JavaWebApplication.java
+++ src/main/java/com/javaweb/Java_web/JavaWebApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebApplication.class, args);
+    }
 
 }
